192290a64d3a5cee0658812b03e53f353db56147
espriggs <spriggs5@gmail.com>
2020-02-07 12:34:45 -0500

    Fix error messages for locations

--- autumncast1.py
+++ autumncast1.py
@@ -15,6 +15,7 @@ from matplotlib.colors import LinearSegmentedColormap
 from matplotlib.patches import Patch
 from shapely.geometry import Point
 from matplotlib.lines import Line2D
+import re
 ########################################################################
 # Read in useful functions
 #######################################################################
@@ -159,120 +160,130 @@ st.title('Autumncast')
 #create some default locations for a dropdown menu:
 select = st.selectbox('Select a location:', ['Cambridge, MA', 'Providence, RI', 'Lee, MA', 'Baxter, Maine', 'Burlington, VT', 'Hanover, NH'])
 city_list = pd.read_csv('backup_city_list.csv', sep='\t')
-#st.write(city_list[city_list.City == select,])
-#st.write(row)
+city_list = pd.read_csv('backup_city_list.csv', sep='\t')
+y = float(city_list[city_list.City == select].Latitude)
+x = float(city_list[city_list.City == select].Longitude)
 
 #for these dropdown menu options, provide some lat lons:
 #this is necessary because sometimes geocoder goes down:
 user_input = st.text_input("Or try searching one here:", "")
 if user_input == '':
     user_input = select
-
-geolocator = Nominatim(user_agent="my-application")
-try:
-    location = geolocator.geocode(user_input)
-    #print('Coordinates: ', location.latitude, location.longitude)
-    x = location.longitude
-    y = location.latitude
-
-except:
-    st.write("Sorry, the feature we use to find locations isn't working right now. Try picking an option from the drop down menu or try again later.")
-    y = 44.483752
-    x = -73.208798
-
-#In order to find the dominant species near the given location,
-#Find the FIP code for the location
-#Encode parameters that are used for the query:
-params = urllib.parse.urlencode({'latitude': y, 'longitude':x, 'format':'json'})
-#Contruct request URL
-url = 'https://geo.fcc.gov/api/census/block/find?' + params
-#Get response from API
-response = requests.get(url)
-#Parse json in response
-data = response.json()
-
-#For that FIPS code, look up the dominant species in the table made from FIA data
-#then print that value for the user (could be extended to show more than one species)
-fips = data['County']['FIPS']
-deciduous_single = pd.read_csv('Single_deciduous_county.csv')
-
-#not all of the counties have FIA plots, for those, do not print a species to the screen
-if any(deciduous_single.COUNTYFIP.astype(str).str.contains(fips)):
-    tree = deciduous_single[deciduous_single.COUNTYFIP == int(fips)].Dominant_Species
-    st.write('The dominant deciduous tree species near ',user_input ,' is ', tree.to_string(index = False), '.', sep='')
-
-#Use the function from above to get the relevant features for the model:
-values = foliage_prediction_2020(x, y)
-
-#Convert these values into inputs for the model:
-model_in = [values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8], values[9], '0', '0', '0', '0', '0']
-
-#add in the species data:
-
-prediction = pickle_model.predict(np.array(model_in).reshape(1,-1))[0]
-start_date = prediction - 7
-end_date = prediction + 7
-
-start_month = pd.to_datetime(start_date, format = '%j').month
-start_day = pd.to_datetime(start_date, format = '%j').day
-
-end_month = pd.to_datetime(end_date, format = '%j').month
-end_day = pd.to_datetime(end_date, format = '%j').day
-
-st.write('The best fall color in', user_input, ' will be between:')
-st.write(calendar.month_name[start_month], str(start_day), str(2020), 'and', calendar.month_name[end_month], str(end_day), str(2020))
+else:
+    geolocator = Nominatim(user_agent="my-application")
+    try:
+        location = geolocator.geocode(user_input)
+        #print('Coordinates: ', location.latitude, location.longitude)
+        x = location.longitude
+        y = location.latitude
+        r = re.compile(r'\bRhode\b | \bConnecticut\b | \bMaine\b | \bHampshire\b | \bVermont\b | \bMassachusetts\b', flags=re.I | re.X)
+        found = r.findall(str(location))
+        if len(found) == 0:
+            st.write("That location wasn't found. Try another place in New England")
+            x = 0
+            y = 0
+    except:
+        st.write("Sorry, the feature we use to find locations isn't working right now. Try picking an option from the drop down menu or try again later.")
+        x = 0
+        y = 0
 
 
-# ##################################################################
-## Plot a heatmap
 
-prediction_date = pd.to_datetime(prediction, format = '%j')
-prediction_date = prediction_date.replace(year = 2020)
-User_input_day = st.date_input('Change this date to see a map for a different date:', prediction_date)
-User_input_day = User_input_day.timetuple().tm_yday
-#User_input_day = st.slider('Day of the year', 200, 365, int(prediction))
-#st.write(str(User_input_day), 'is ', calendar.month_name[pd.to_datetime(User_input_day, format = '%j').month], str(pd.to_datetime(User_input_day, format = '%j').day), '2020')
-#NE4.head()
+if y > 0:
 
-#create a custom color list that varies based on the user input:
-colors_list = np.repeat('lightgrey', 9, axis = 0)
-dates = [270, 275, 280, 285, 290, 295, 300, 305, 310]
-i = 0
-for date in dates:
-   # print(User_input_day-date)
-    if (User_input_day-date <= 14) & (User_input_day-date >= 7):
-        colors_list[i] = 'darkred'
-    if (User_input_day-date <= 7) & (User_input_day-date >= -7):
-        colors_list[i] = 'red'
-    if (User_input_day-date >= -14) & (User_input_day-date <= -7):
-        colors_list[i] = 'orange'
-    if (User_input_day-date >= -21) & (User_input_day-date <= -14):
-        colors_list[i] = 'goldenrod'
-    i +=1
+    #In order to find the dominant species near the given location,
+    #Find the FIP code for the location
+    #Encode parameters that are used for the query:
+    params = urllib.parse.urlencode({'latitude': y, 'longitude':x, 'format':'json'})
+    #Contruct request URL
+    url = 'https://geo.fcc.gov/api/census/block/find?' + params
+    #Get response from API
+    response = requests.get(url)
+    #Parse json in response
+    data = response.json()
 
-#color_list = ['darkred','darkred','darkred','red','red','red','orange','orange','lightgrey']
-colormap = []
-colormap = LinearSegmentedColormap.from_list([270, 275, 280, 285, 290, 295, 300, 305, 310],colors_list)
+    #For that FIPS code, look up the dominant species in the table made from FIA data
+    #then print that value for the user (could be extended to show more than one species)
+    fips = data['County']['FIPS']
+    deciduous_single = pd.read_csv('Single_deciduous_county.csv')
 
-add_point = Point((x,y))
-gdf_am = gpd.GeoSeries([add_point], crs={'init': 'epsg:4326'})
+    #not all of the counties have FIA plots, for those, do not print a species to the screen
+    if any(deciduous_single.COUNTYFIP.astype(str).str.contains(fips)):
+        tree = deciduous_single[deciduous_single.COUNTYFIP == int(fips)].Dominant_Species
+        st.write('The dominant deciduous tree species near ',user_input ,' is ', tree.to_string(index = False), '.', sep='')
 
-fig, ax = plt.subplots(figsize=(8,8))
-NE4.plot(ax=ax, column = 'predicted', cmap = colormap)
-gdf_am.to_crs({'init': 'epsg:4326'}).plot(ax=ax, markersize = 150, marker = '*')
+    #Use the function from above to get the relevant features for the model:
+    values = foliage_prediction_2020(x, y)
 
-#ax = NE3.plot(column = 'predicted', cmap = colormap)
-#legend_labels = {'goldenrod':'very early','orange':'early', 'red': 'peak', 'darkred':'late'}
-legend_elements = [Patch(facecolor = 'lightgrey', edgecolor ='w', label = 'No fall color'),
-                    Patch(facecolor = 'goldenrod', edgecolor ='w', label = 'Very early'),
-                   Patch(facecolor = 'orange', edgecolor ='w', label = 'Early'),
-                   Patch(facecolor = 'red', edgecolor ='w', label = 'Peak'),
-                   Patch(facecolor = 'darkred', edgecolor ='w', label = 'Late'),
-                   Line2D([0],[0], marker = '*',
-                    color ='w', markerfacecolor='cornflowerblue',
-                    label = user_input, markersize=20)]
-ax.legend(handles=legend_elements,
-          bbox_to_anchor=(.3, .97),
-          facecolor="white",
-          frameon = False)
-st.pyplot()
+    #Convert these values into inputs for the model:
+    model_in = [values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8], values[9], '0', '0', '0', '0', '0']
+
+    #add in the species data:
+
+    prediction = pickle_model.predict(np.array(model_in).reshape(1,-1))[0]
+    start_date = prediction - 7
+    end_date = prediction + 7
+
+    start_month = pd.to_datetime(start_date, format = '%j').month
+    start_day = pd.to_datetime(start_date, format = '%j').day
+
+    end_month = pd.to_datetime(end_date, format = '%j').month
+    end_day = pd.to_datetime(end_date, format = '%j').day
+
+    st.write('The best fall color in', user_input, ' will be between:')
+    st.write(calendar.month_name[start_month], str(start_day), str(2020), 'and', calendar.month_name[end_month], str(end_day), str(2020))
+
+
+    # ##################################################################
+    ## Plot a heatmap
+
+    prediction_date = pd.to_datetime(prediction, format = '%j')
+    prediction_date = prediction_date.replace(year = 2020)
+    User_input_day = st.date_input('Change this date to see a map for a different date:', prediction_date)
+    User_input_day = User_input_day.timetuple().tm_yday
+    #User_input_day = st.slider('Day of the year', 200, 365, int(prediction))
+    #st.write(str(User_input_day), 'is ', calendar.month_name[pd.to_datetime(User_input_day, format = '%j').month], str(pd.to_datetime(User_input_day, format = '%j').day), '2020')
+    #NE4.head()
+
+    #create a custom color list that varies based on the user input:
+    colors_list = np.repeat('lightgrey', 9, axis = 0)
+    dates = [270, 275, 280, 285, 290, 295, 300, 305, 310]
+    i = 0
+    for date in dates:
+       # print(User_input_day-date)
+        if (User_input_day-date <= 14) & (User_input_day-date >= 7):
+            colors_list[i] = 'darkred'
+        if (User_input_day-date <= 7) & (User_input_day-date >= -7):
+            colors_list[i] = 'red'
+        if (User_input_day-date >= -14) & (User_input_day-date <= -7):
+            colors_list[i] = 'orange'
+        if (User_input_day-date >= -21) & (User_input_day-date <= -14):
+            colors_list[i] = 'goldenrod'
+        i +=1
+
+    #color_list = ['darkred','darkred','darkred','red','red','red','orange','orange','lightgrey']
+    colormap = []
+    colormap = LinearSegmentedColormap.from_list([270, 275, 280, 285, 290, 295, 300, 305, 310],colors_list)
+
+    add_point = Point((x,y))
+    gdf_am = gpd.GeoSeries([add_point], crs={'init': 'epsg:4326'})
+
+    fig, ax = plt.subplots(figsize=(8,8))
+    NE4.plot(ax=ax, column = 'predicted', cmap = colormap)
+    gdf_am.to_crs({'init': 'epsg:4326'}).plot(ax=ax, markersize = 150, marker = '*')
+
+    #ax = NE3.plot(column = 'predicted', cmap = colormap)
+    #legend_labels = {'goldenrod':'very early','orange':'early', 'red': 'peak', 'darkred':'late'}
+    legend_elements = [Patch(facecolor = 'lightgrey', edgecolor ='w', label = 'No fall color'),
+                        Patch(facecolor = 'goldenrod', edgecolor ='w', label = 'Very early'),
+                       Patch(facecolor = 'orange', edgecolor ='w', label = 'Early'),
+                       Patch(facecolor = 'red', edgecolor ='w', label = 'Peak'),
+                       Patch(facecolor = 'darkred', edgecolor ='w', label = 'Late'),
+                       Line2D([0],[0], marker = '*',
+                        color ='w', markerfacecolor='cornflowerblue',
+                        label = user_input, markersize=20)]
+    ax.legend(handles=legend_elements,
+              bbox_to_anchor=(.3, .97),
+              facecolor="white",
+              frameon = False)
+    st.pyplot()
